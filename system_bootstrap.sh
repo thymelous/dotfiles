85b7ef4f6978a721551a5414a6678df0d760fed7
thymelous <tim.labushev@gmail.com>
2018-03-10 13:23:52 +0300

    refine the bootstrapping script

--- system_bootstrap.sh
+++ system_bootstrap.sh
@@ -7,11 +7,20 @@ sudo dnf -y install curl autoconf automake git
 sudo dnf install --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install --nogpgcheck http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-# Fetch & run the rest of the scripts
+# Fetch the rest of the scripts
 mkdir -p ~/git
 cd ~/git
 
 git clone https://github.com/thymelous/dotfiles.git
-cd dotfiles/system
+cd dotfiles
 
+# Link dotfiles
+rm -i ~/.bashrc; ln -s `pwd`/.bashrc     ~/.bashrc
+rm -i ~/.vimrc;  ln -s `pwd`/.vimrc      ~/.vimrc
+mkdir -p ~/.vim; ln -s `pwd`/plugins.vim ~/.vim/plugins.vim
+ln -s `pwd`/gitignore_global ~/gitignore_global
+git config --global core.excludesfile ~/gitignore_global
+
+# Continue installation
+cd system
 run-parts .
