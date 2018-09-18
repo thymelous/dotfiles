1b5734dd3372ed629d8513c3d0c1c68e58466e1e
thymelous <tim.labushev@gmail.com>
2018-09-18 09:00:19 +0300

    use the distribution version of texlive

--- system/5-texlive.sh
+++ system/5-texlive.sh
@@ -1,32 +1,6 @@
 #!/bin/bash
 
-mkdir -p /tmp/texlive_inst
+sudo dnf install -y texlive texlive-xetex \
+  texlive-babel-russian texlive-hyphen-russian
 
-cat texlive.profile | sed -e "s|~|$HOME|" > /tmp/texlive_inst/texlive.profile
-
-cd /tmp/texlive_inst
-
-# You may need to change the exclusion patterns based on your system and arch
-rsync -av --progress                 \
-  --exclude '*.exe'                  \
-  --exclude '*.tar'                  \
-  --exclude '*.doc.tar.xz'           \
-  --exclude '*.source.tar.xz'        \
-  --exclude '*-cygwin.tar.xz*'       \
-  --exclude '*.win32.tar.xz'         \
-  --exclude '*-darwin.tar.xz'        \
-  --exclude '*-darwinlegacy.tar.xz'  \
-  --exclude '*-solaris.tar.xz'       \
-  --exclude '*-netbsd.tar.xz'        \
-  --exclude '*-freebsd.tar.xz'       \
-  --exclude '*.armel-linux.tar.xz'   \
-  --exclude '*.armhf-linux.tar.xz'   \
-  --exclude '*.i386-linux.tar.xz'    \
-  --exclude '*.powerpc-linux.tar.xz' \
-  rsync://mirrors.mi.ras.ru/CTAN/systems/texlive/tlnet/ .
-
-chmod +x ./install-tl
-./install-tl -repository . -profile texlive.profile
-
-cd ..
-rm -r texlive_inst
+sudo dnf install -y http://ftp.gwdg.de/pub/opensuse/repositories/home:/zhonghuaren/Fedora_`rpm -E %fedora`/noarch/cm-unicode-fonts-0.7.0-191.1.noarch.rpm
