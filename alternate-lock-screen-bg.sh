1fec5ad2d3437d9aaca977c370a6d495b40b9134
timlathy <tim.labushev@gmail.com>
2020-05-22 11:34:51 +0300

    add a script for alternating lock screen bg

---
+++ alternate-lock-screen-bg.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# Requires https://github.com/sunwxg/gnome-shell-extension-unlockDialogBackground
+# on GNOME 3.36
+
+# Example usage:
+# crontab -e
+# */15 * * * * /path/to/alternate-lock-screen-bg.sh
+
+WALLPAPER_DIR=~/Pictures/Wallpapers
+LRU_WALLPAPER="$WALLPAPER_DIR/$(ls -tr $WALLPAPER_DIR | grep -E "(png|jpg)$" | head -n 1)"
+BGEXT_SCHEMADIR=~/.local/share/gnome-shell/extensions/unlockDialogBackground@sun.wxg@gmail.com/schemas
+
+touch "$LRU_WALLPAPER"
+
+gsettings set org.gnome.desktop.screensaver picture-uri "$LRU_WALLPAPER"
+gsettings --schemadir $BGEXT_SCHEMADIR \
+  set org.gnome.shell.extensions.unlockDialogBackground picture-uri "$LRU_WALLPAPER"
